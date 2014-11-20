3313a82e134c548c1ef34a4d6af31822903d9d96
Jake Barszcz <jfbarszcz@gmail.com>
2014-11-20 11:12:29 -0500

    Added lots of move behavior

--- board.rb
+++ board.rb
@@ -1,3 +1,5 @@
+require_relative 'piece'
+
 class Board
 
   def [](pos)
@@ -15,6 +17,26 @@ class Board
     set_pieces if start
   end
 
+  def set_pieces
+    # These go over 80 chars but for the sake of readability they're one-liners
+    @grid[0].map.with_index { |s, i| i.odd? ? Piece.new([0, i], :red, self) : nil }
+    @grid[1].map.with_index { |s, i| i.even? ? Piece.new([1, i], :red, self) : nil }
+    @grid[2].map.with_index { |s, i| i.odd? ? Piece.new([2, i], :red, self) : nil }
+    @grid[5].map.with_index { |s, i| i.even? ? Piece.new([5, i], :black, self) : nil }
+    @grid[6].map.with_index { |s, i| i.odd? ? Piece.new([6, i], :black, self) : nil }
+    @grid[7].map.with_index { |s, i| i.even? ? Piece.new([7, i], :black, self) : nil }
+  end
 
+  def update(start_pos, end_pos)
+    self[end_pos], self[start_pos] = self[start_pos], nil
+  end
+
+  def capture(pos)
+    self[pos] = nil
+  end
+
+  def empty?(pos)
+    self[pos].nil?
+  end
 
 end
--- piece.rb
+++ piece.rb
@@ -6,23 +6,94 @@ class Piece
     @pos, @color, @board, @king = pos, color, board, king
   end
 
-  def perform_slide
-    diffs = slide_diffs
-    moves = diffs.map do |diff|
-      [diff[0] + pos[0], diff[1] + pos[1]]
+  def perform_move(new_pos)
+    slide_moves = get_moves(slide_diffs)
+    jump_moves = get_moves(jump_diffs)
+
+    #reassignment so I don't screw with the indexes
+    jump_moves = jump_moves.select.with_index do |move, i|
+      jumped_piece = @board[slide_moves[i]]
+      jumped_piece && jumped_piece.color != color
     end
-    moves.select! do |move|
-      move.all? { |coord| coord.between?(0, 7) } &&
-      board[move].nil?
+
+    if jump_moves.include?(new_pos) && board.empty?(new_pos)
+      perform_jump!(new_pos)
+
+    elsif slide_moves.include?(new_pos) && board.empty?(new_pos)
+      perform_slide!(new_pos)
+
+    else
+      return false
     end
+
+    true
+
   end
 
-  def perform_jump
-
+  def perform_slide!(new_pos)
+    @board.update(@pos, new_pos)
+    @pos = new_pos
   end
+
+  def perform_jump!(new_pos)
+    #kind of a crappy hack
+    jumped_pos = [pos, new_pos].transpose.map { |coords| coords.inject(:+) / 2 }
+
+    @board.update(@pos, new_pos)
+    @board.capture(jumped_pos)
+    @pos = new_pos
+  end
+
+  # def perform_slide(new_pos)
+  #   diffs = slide_diffs
+  #
+  #   moves = diffs.map do |diff|
+  #     [diff[0] + pos[0], diff[1] + pos[1]]
+  #   end
+  #
+  #   moves.select! do |move|
+  #     move.all? { |coord| coord.between?(0, 7) } &&
+  #     board[move].nil?
+  #   end
+  #
+  #   return false unless moves.include?(new_pos)
+  #
+  #   @board.update(@pos, new_pos)
+  #   @pos = new_pos
+  #   true
+  # end
+  #
+  # def perform_jump
+  #   diffs = jump_diffs
+  #
+  #   moves = diffs.map do |diff|
+  #     [diff[0] + pos[0], diff[1] + pos[1]]
+  #   end
+  #
+  #   moves.select! do |move|
+  #     move.all? { |coord| coord.between?(0, 7) } &&
+  #     board[move].nil?
+  #   end
+  #
+  #   return false unless moves.include?(new_pos)
+  #
+  #   @board.update(@pos, new_pos)
+  #   @pos = new_pos
+  #
+  # end
+
+
 
   def is_king?
     @king
+  end
+
+  def get_moves(diffs)
+    moves = diffs.map { |diff| [diff[0] + pos[0], diff[1] + pos[1]] }
+    moves.select do |move|
+      move.all? { |coord| coord.between?(0, 7) } # &&
+      #board[move].nil?
+    end
   end
 
   def slide_diffs
@@ -45,21 +116,25 @@ class Piece
   end
 
   def jump_diffs
-    red_jump_diffs = [
-      [2, -2],
-      [2, 2]
-    ]
 
-    black_jump_diffs = [
-      [-2, -2],
-      [-2, 2]
-    ]
-
-    if is_king?
-      red_jump_diffs + black_jump-diffs
-    else
-      color == :red ? red_slide_diffs : black_slide_diffs
+    slide_diffs.map do |diff|
+      diff.map { |coord| coord * 2 }
     end
-  end
+  #   red_jump_diffs = [
+  #     [2, -2],
+  #     [2, 2]
+  #   ]
+  #
+  #   black_jump_diffs = [
+  #     [-2, -2],
+  #     [-2, 2]
+  #   ]
+  #
+  #   if is_king?
+  #     red_jump_diffs + black_jump-diffs
+  #   else
+  #     color == :red ? red_slide_diffs : black_slide_diffs
+  #   end
+   end
 
 end
